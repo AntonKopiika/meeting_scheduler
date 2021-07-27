c18099ef5f521b417e81102ec5a4aa1e93ca3215
AntonKopiika <toxxa099@gmail.com>
2021-07-27 11:23:22 +0300

    change dockerhub push file

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,6 +5,6 @@ else
 TAG="$TRAVIS_BRANCH"
 fi
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-docker tag $TRAVIS_REPO_SLUG $DOCKER_REPO
+docker tag $TRAVIS_REPO_SLUG $DOCKER_USER/$DOCKER_REPO
 docker login --username $DOCKER_USER --password $DOCKER_PASS
-docker push $DOCKER_REPO
+docker push $DOCKER_USER/$DOCKER_REPO
