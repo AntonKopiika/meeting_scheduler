039130395eb41dcc5cbe23aaf4b6bd971c6be7e9
AntonKopiika <toxxa099@gmail.com>
2021-08-06 11:37:43 +0300

    add ssl_context

--- meeting_scheduler/wsgi.py
+++ meeting_scheduler/wsgi.py
@@ -96,4 +96,4 @@ def _get_token_from_cache(scope=None):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.getenv('PORT', 5000)))
+    app.run(host="0.0.0.0", port=int(os.getenv('PORT', 5000)), ssl_context="adhoc")
