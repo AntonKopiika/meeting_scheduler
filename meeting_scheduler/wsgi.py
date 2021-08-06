90f48f6e249f375d9e5b4be261bab7b520468fba
AntonKopiika <toxxa099@gmail.com>
2021-08-06 17:03:21 +0300

    add redirect uri

--- meeting_scheduler/wsgi.py
+++ meeting_scheduler/wsgi.py
@@ -85,7 +85,7 @@ def _build_msal_app(cache=None, authority=None):
 def _build_auth_code_flow(authority=None, scopes=None):
     return _build_msal_app(authority=authority).initiate_auth_code_flow(
         scopes or [],
-        redirect_uri=url_for("authorized", _external=True))
+        redirect_uri="https://www.mymeeeting.com/getAToken")
 
 
 def _get_token_from_cache(scope=None):
